8efd35a489eaddcd689a32779d2656796db97823
Park Hanna <qkrgkssk8087@gmail.com>
2022-08-02 23:51:51 +0900

    first commit

---
+++ app/src/main/java/com/example/myapplication/Test.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+fun main(args:Array<String>){
+    var n : Int=10
+    val number1 : Int=10
+    var number2 = 20
+    val name = "kotlin"
+    var subject : String="java"
+
+    subject = "Kotlin"
+
+    println("number1: $number1")
+    println("number2: $number2")
+    println("name : $name")
+    println("subject : $subject")
+    println(n)
+    println("Hello World!")
+}
+
